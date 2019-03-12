f1a4ec9e3f034cf170b762eb1c1fef449f0a0e00
shy871 <shy871@163.com>
2019-03-12 09:58:18 +0800

    prepare proxy pattern

---
+++ design-pattern-demo/src/main/java/com/izkml/shy/stucturetype/proxy/ProxyMain.java
@@ -0,0 +1,16 @@
+package com.izkml.shy.stucturetype.proxy;
+
+/**
+ * @author: shy
+ * @description: 代理模式
+ * @create: 2019-03-12 09:53
+ **/
+
+public class ProxyMain {
+
+    public static void main(String[] args){
+
+
+    }
+
+}
