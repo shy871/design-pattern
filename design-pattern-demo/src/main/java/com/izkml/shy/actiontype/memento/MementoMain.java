79eb5daa77731bcba919ac368c735b7a19826430
shy871 <shy871@163.com>
2019-03-22 16:43:29 +0800

    xxx

---
+++ design-pattern-demo/src/main/java/com/izkml/shy/actiontype/memento/MementoMain.java
@@ -0,0 +1,16 @@
+package com.izkml.shy.actiontype.memento;
+
+/**
+ * @author: shy
+ * @description: 备忘录模式
+ * @create: 2019-03-20 13:34
+ **/
+
+public class MementoMain {
+
+    public static void main(String[] args){
+
+    }
+
+
+}
